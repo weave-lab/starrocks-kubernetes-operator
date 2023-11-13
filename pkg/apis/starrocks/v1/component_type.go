ff82f6b912fee58b78be0f15e48f9b274b188827
devkapil421 <kapil.dev@getweave.com>
2023-11-13 20:44:46 +0530

    adding crds file

--- pkg/apis/starrocks/v1/component_type.go
+++ pkg/apis/starrocks/v1/component_type.go
@@ -14,138 +14,134 @@
  * limitations under the License.
  */
 
- package v1
+package v1
 
- import corev1 "k8s.io/api/core/v1"
- 
- type StarRocksComponentSpec struct {
-	 StarRocksLoadSpec `json:",inline"`
- 
-	 // RunAsNonRoot is used to determine whether to run starrocks as a normal user.
-	 // If RunAsNonRoot is true, operator will set RunAsUser and RunAsGroup to 1000 in securityContext.
-	 // default: nil
-	 RunAsNonRoot *bool `json:"runAsNonRoot,omitempty"`
- 
-	 UserID *int64 `json:"userID,omitempty"`
- 
-	 GroupID *int64 `json:"groupID,omitempty"`
- 
-	 AllowPrivilegeEscalation *bool `json:"allowPrivilegeEscalation"`
-	 ReadOnlyRootFilesystem   *bool `json:"readOnlyRootFilesystem"`
-	 // the reference for configMap which allow users to mount any files to container.
-	 // +optional
-	 ConfigMaps []ConfigMapReference `json:"configMaps,omitempty"`
- 
-	 // the reference for secrets.
-	 // +optional
-	 Secrets []SecretReference `json:"secrets,omitempty"`
- 
-	 // HostAliases is an optional list of hosts and IPs that will be injected into the pod's hosts
-	 // file if specified. This is only valid for non-hostNetwork pods.
-	 // +optional
-	 HostAliases []corev1.HostAlias `json:"hostAliases,omitempty"`
- 
-	 // TerminationGracePeriodSeconds defines duration in seconds the pod needs to terminate gracefully. May be decreased in delete request.
-	 // Value must be non-negative integer. The value zero indicates stop immediately via
-	 // the kill signal (no opportunity to shut down).
-	 // If this value is nil, the default grace period will be used instead.
-	 // The grace period is the duration in seconds after the processes running in the pod are sent
-	 // a termination signal and the time when the processes are forcibly halted with a kill signal.
-	 // Set this value longer than the expected cleanup time for your process.
-	 // Defaults to 120 seconds.
-	 // +optional
-	 TerminationGracePeriodSeconds *int64 `json:"terminationGracePeriodSeconds,omitempty"`
- }
- 
- // StarRocksComponentStatus represents the status of a starrocks component.
- type StarRocksComponentStatus struct {
-	 // the name of fe service exposed for user.
-	 ServiceName string `json:"serviceName,omitempty"`
- 
-	 // FailedInstances failed pod names.
-	 FailedInstances []string `json:"failedInstances,omitempty"`
- 
-	 // CreatingInstances in creating pod names.
-	 CreatingInstances []string `json:"creatingInstances,omitempty"`
- 
-	 // RunningInstances in running status pod names.
-	 RunningInstances []string `json:"runningInstances,omitempty"`
- 
-	 // ResourceNames the statefulset names of fe in v1alpha1 version.
-	 ResourceNames []string `json:"resourceNames,omitempty"`
- 
-	 // Phase the value from all pods of component status. If component have one failed pod phase=failed,
-	 // also if fe have one creating pod phase=creating, also if component all running phase=running, others unknown.
-	 Phase MemberPhase `json:"phase"`
- 
-	 // +optional
-	 // Reason represents the reason of not running.
-	 Reason string `json:"reason"`
- }
- 
- type ConfigMapInfo struct {
-	 // the config info for start progress.
-	 ConfigMapName string `json:"configMapName,omitempty"`
- 
-	 // the config response key in configmap.
-	 ResolveKey string `json:"resolveKey,omitempty"`
- }
- 
- type ConfigMapReference MountInfo
- 
- type SecretReference MountInfo
- 
- type MountInfo struct {
-	 // This must match the Name of a ConfigMap or Secret in the same namespace, and
-	 // the length of name must not more than 50 characters.
-	 Name string `json:"name,omitempty"`
- 
-	 // Path within the container at which the volume should be mounted.  Must
-	 // not contain ':'.
-	 MountPath string `json:"mountPath,omitempty"`
- 
-	 // SubPath within the volume from which the container's volume should be mounted.
-	 // Defaults to "" (volume's root).
-	 SubPath string `json:"subPath,omitempty"`
- }
- 
- func (spec *StarRocksComponentSpec) GetHostAliases() []corev1.HostAlias {
-	 return spec.HostAliases
- }
- 
- func (spec *StarRocksComponentSpec) GetRunAsNonRoot() (*int64, *int64) {
-	 runAsNonRoot := spec.RunAsNonRoot
-	 if runAsNonRoot == nil || !*runAsNonRoot {
-		 return nil, nil
-	 }
- 
-	 var userID int64 = 1000
-	 var groupID int64 = 1000
- 
-	 if spec.UserID != nil && *spec.UserID > 0 {
-		 userID = *spec.UserID
-	 }
-	 if spec.GroupID != nil && *spec.GroupID > 0 {
-		 groupID = *spec.GroupID
-	 }
- 
-	 return &userID, &groupID
- }
- 
- func (spec *StarRocksComponentSpec) GetAllowPrivilegeEscalation() *bool {
-	 allowPrivilegeEscalation := spec.AllowPrivilegeEscalation
-	 return allowPrivilegeEscalation
- }
- 
- func (spec *StarRocksComponentSpec) GetReadOnlyRootFilesystem() *bool {
-	 readOnlyRootFilesystem := spec.ReadOnlyRootFilesystem
-	 return readOnlyRootFilesystem
- }
- 
- func (spec *StarRocksComponentSpec) GetTerminationGracePeriodSeconds() *int64 {
-	 var defaultSeconds int64 = 120
-	 if spec.TerminationGracePeriodSeconds == nil {
-		 return &defaultSeconds
-	 }
-	 return spec.TerminationGracePeriodSeconds
- }
+import corev1 "k8s.io/api/core/v1"
+
+type StarRocksComponentSpec struct {
+	StarRocksLoadSpec `json:",inline"`
+
+	// RunAsNonRoot is used to determine whether to run starrocks as a normal user.
+	// If RunAsNonRoot is true, operator will set RunAsUser and RunAsGroup to 1000 in securityContext.
+	// default: nil
+	RunAsNonRoot *bool `json:"runAsNonRoot,omitempty"`
+    UserID *int64 `json:"userID,omitempty"`
+	GroupID *int64 `json:"groupID,omitempty"`
+	AllowPrivilegeEscalation *bool `json:"allowPrivilegeEscalation"`
+	ReadOnlyRootFilesystem   *bool `json:"readOnlyRootFilesystem"`
+	// the reference for configMap which allow users to mount any files to container.
+	// +optional
+	ConfigMaps []ConfigMapReference `json:"configMaps,omitempty"`
+
+	// the reference for secrets.
+	// +optional
+	Secrets []SecretReference `json:"secrets,omitempty"`
+
+	// HostAliases is an optional list of hosts and IPs that will be injected into the pod's hosts
+	// file if specified. This is only valid for non-hostNetwork pods.
+	// +optional
+	HostAliases []corev1.HostAlias `json:"hostAliases,omitempty"`
+
+	// TerminationGracePeriodSeconds defines duration in seconds the pod needs to terminate gracefully. May be decreased in delete request.
+	// Value must be non-negative integer. The value zero indicates stop immediately via
+	// the kill signal (no opportunity to shut down).
+	// If this value is nil, the default grace period will be used instead.
+	// The grace period is the duration in seconds after the processes running in the pod are sent
+	// a termination signal and the time when the processes are forcibly halted with a kill signal.
+	// Set this value longer than the expected cleanup time for your process.
+	// Defaults to 120 seconds.
+	// +optional
+	TerminationGracePeriodSeconds *int64 `json:"terminationGracePeriodSeconds,omitempty"`
+}
+
+// StarRocksComponentStatus represents the status of a starrocks component.
+type StarRocksComponentStatus struct {
+	// the name of fe service exposed for user.
+	ServiceName string `json:"serviceName,omitempty"`
+
+	// FailedInstances failed pod names.
+	FailedInstances []string `json:"failedInstances,omitempty"`
+
+	// CreatingInstances in creating pod names.
+	CreatingInstances []string `json:"creatingInstances,omitempty"`
+
+	// RunningInstances in running status pod names.
+	RunningInstances []string `json:"runningInstances,omitempty"`
+
+	// ResourceNames the statefulset names of fe in v1alpha1 version.
+	ResourceNames []string `json:"resourceNames,omitempty"`
+
+	// Phase the value from all pods of component status. If component have one failed pod phase=failed,
+	// also if fe have one creating pod phase=creating, also if component all running phase=running, others unknown.
+	Phase MemberPhase `json:"phase"`
+
+	// +optional
+	// Reason represents the reason of not running.
+	Reason string `json:"reason"`
+}
+
+type ConfigMapInfo struct {
+	// the config info for start progress.
+	ConfigMapName string `json:"configMapName,omitempty"`
+
+	// the config response key in configmap.
+	ResolveKey string `json:"resolveKey,omitempty"`
+}
+
+type ConfigMapReference MountInfo
+
+type SecretReference MountInfo
+
+type MountInfo struct {
+	// This must match the Name of a ConfigMap or Secret in the same namespace, and
+	// the length of name must not more than 50 characters.
+	Name string `json:"name,omitempty"`
+
+	// Path within the container at which the volume should be mounted.  Must
+	// not contain ':'.
+	MountPath string `json:"mountPath,omitempty"`
+
+	// SubPath within the volume from which the container's volume should be mounted.
+	// Defaults to "" (volume's root).
+	SubPath string `json:"subPath,omitempty"`
+}
+
+func (spec *StarRocksComponentSpec) GetHostAliases() []corev1.HostAlias {
+	return spec.HostAliases
+}
+
+func (spec *StarRocksComponentSpec) GetRunAsNonRoot() (*int64, *int64) {
+	runAsNonRoot := spec.RunAsNonRoot
+	if runAsNonRoot == nil || !*runAsNonRoot {
+		return nil, nil
+	}
+
+	var userID int64 = 1000
+	var groupID int64 = 1000
+
+	if spec.UserID != nil && *spec.UserID > 0 {
+		userID = *spec.UserID
+	}
+	if spec.GroupID != nil && *spec.GroupID > 0 {
+		groupID = *spec.GroupID
+	}
+	return &userID, &groupID
+}
+
+func (spec *StarRocksComponentSpec) GetTerminationGracePeriodSeconds() *int64 {
+	var defaultSeconds int64 = 120
+	if spec.TerminationGracePeriodSeconds == nil {
+		return &defaultSeconds
+	}
+	return spec.TerminationGracePeriodSeconds
+}
+
+func (spec *StarRocksComponentSpec) GetAllowPrivilegeEscalation() *bool {
+	allowPrivilegeEscalation := spec.AllowPrivilegeEscalation
+	return allowPrivilegeEscalation
+}
+
+func (spec *StarRocksComponentSpec) GetReadOnlyRootFilesystem() *bool {
+	readOnlyRootFilesystem := spec.ReadOnlyRootFilesystem
+	return readOnlyRootFilesystem
+}
